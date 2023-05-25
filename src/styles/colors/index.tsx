14fef30b2dff9ca8557be6082b93f821eb5113d0
João <joao.donghia@gmail.com>
2023-05-25 20:40:00 -0300

    feat(styles): creating global style provider

---
+++ src/styles/colors/index.tsx
@@ -0,0 +1,16 @@
+const COLORS_THEME_PROVIDER = {
+  "noble-black-200": "#CDCECF",
+  "noble-black-300": "#9B9C9E",
+  "noble-black-400": "#686B6E",
+  "noble-black-500": "#363A3D",
+  "noble-black-600": "#1A1D21",
+  "gradient-blue": "linear-gradient(#82DBF7, #B6F09C)",
+  "stem-green-500": "#B6F09C",
+  "day-blue-900": "#0C1132",
+} as object;
+
+const COLORS_GLOBAL_STYLE = `
+    --color-noble-black-700: #131619;
+  ` as string;
+
+export { COLORS_THEME_PROVIDER, COLORS_GLOBAL_STYLE };
